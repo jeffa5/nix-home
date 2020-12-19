b5c17206eba9f165eefe2a4041db3882ad553717
Andrew Jeffery <dev@jeffas.io>
2020-12-19 17:15:22 +0000

    Allow unfree

--- flake.nix
+++ flake.nix
@@ -7,22 +7,15 @@
   };
 
   outputs = { home-manager, nixpkgs, ... }:
-    let
-        nixpkgs = import nixpkgs {
-          config = { allowUnfree = true; };
-        };
-      in
   {
 
     nixosConfigurations = {
-      carbide =
-      nixpkgs.lib.nixosSystem {
+      carbide = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./carbide
           home-manager.nixosModules.home-manager
           {
-            home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.andrew = import ./home.nix;
           }
