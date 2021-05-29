dab5c8295649df1d0aa385eb505885a41d7b5363
Andrew Jeffery <dev@jeffas.io>
2021-05-29 20:45:30 +0100

    Revert "Remove github ssh config"
    This reverts commit 63933d31d95b34e3018f29e1c4656b0e5f1e494c.

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,6 +1,10 @@
 {
   enable = true;
   matchBlocks = {
+    "github.com" = {
+      hostname = "ssh.github.com";
+      port = 443;
+    };
     "binky" = {
       hostname = "binky.cl.cam.ac.uk";
       user = "apj39";
