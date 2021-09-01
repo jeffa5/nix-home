5026eb89f6a07cf40680cc0e3736f5a22a993ad9
Andrew Jeffery <dev@jeffas.io>
2021-09-01 21:26:04 +0100

    Remove github ssh fix

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,10 +1,6 @@
 {
   enable = true;
   matchBlocks = {
-    "github.com" = {
-      hostname = "ssh.github.com";
-      port = 443;
-    };
     "binky" = {
       hostname = "binky.cl.cam.ac.uk";
       user = "apj39";
