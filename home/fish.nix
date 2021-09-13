13d03e0fcbdd79793e7a58a28e7ff35085597f3b
Andrew Jeffery <dev@jeffas.io>
2021-09-13 21:04:17 +0100

    Add some fish customisation

--- home/fish.nix
+++ home/fish.nix
@@ -5,4 +5,13 @@ pkgs: {
     cat = "bat";
     ping = "prettyping --nolegend";
   };
+
+  interactiveShellInit = ''
+    set -g fish_greeting # disable welcome message
+    fish_vi_key_bindings
+
+    ${builtins.readFile ./gruvbox-fish.fish}
+
+    base16-gruvbox-dark-medium
+  '';
 }
