b6ab797acdf76cacaf8fc96848ece87d87898818
Andrew Jeffery <dev@jeffas.io>
2022-03-07 17:10:07 +0000

    Enable git lfs

--- home/git.nix
+++ home/git.nix
@@ -2,6 +2,7 @@ pkgs: {
   enable = true;
   package = pkgs.gitAndTools.gitFull;
   delta.enable = true;
+  lfs.enable = true;
   userEmail = "dev@jeffas.io";
   userName = "Andrew Jeffery";
   extraConfig = {
