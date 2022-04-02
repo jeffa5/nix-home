010060146541c697f011e85b5ad97274c851a412
Andrew Jeffery <dev@jeffas.io>
2022-04-02 12:06:56 +0100

    Fix delta theme

--- home/git.nix
+++ home/git.nix
@@ -2,6 +2,12 @@ pkgs: {
   enable = true;
   package = pkgs.gitAndTools.gitFull;
   delta.enable = true;
+  delta.options = {
+    gruvbox-light = {
+      syntax-theme = "gruvbox-light";
+    };
+    features = "gruvbox-light";
+  };
   lfs.enable = true;
   userEmail = "dev@jeffas.io";
   userName = "Andrew Jeffery";
