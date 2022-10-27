c2e9b1de0c7cbca742d4fe1cd465dcae17a89c3b
Andrew Jeffery <dev@jeffas.io>
2022-10-27 21:37:50 +0100

    Tidy up dconf settings

--- home/dconf.nix
+++ home/dconf.nix
@@ -7,22 +7,6 @@ in {
       temperature-unit = "centigrade";
     };
 
-    "org/gnome/Geary" = {
-      folder-list-pane-position-horizontal = 100;
-      messages-pane-position = 351;
-      migrated-config = true;
-    };
-
-    "org/gnome/Music" = {
-      window-maximized = true;
-      window-position = [0 0];
-      window-size = [1920 1006];
-    };
-
-    "org/gnome/desktop/calendar" = {
-      show-weekdate = false;
-    };
-
     "org/gnome/desktop/input-sources" = {
       per-window = false;
       show-all-sources = false;
@@ -48,31 +32,6 @@ in {
       toolbar-style = "text";
     };
 
-    "org/gnome/desktop/notifications" = {
-      application-children = ["thunderbird" "firefox" "zoom" "gnome-control-center" "org-gnome-tweaks"];
-      show-in-lock-screen = false;
-    };
-
-    "org/gnome/desktop/notifications/application/firefox" = {
-      application-id = "firefox.desktop";
-    };
-
-    "org/gnome/desktop/notifications/application/gnome-control-center" = {
-      application-id = "gnome-control-center.desktop";
-    };
-
-    "org/gnome/desktop/notifications/application/org-gnome-tweaks" = {
-      application-id = "org.gnome.tweaks.desktop";
-    };
-
-    "org/gnome/desktop/notifications/application/thunderbird" = {
-      application-id = "thunderbird.desktop";
-    };
-
-    "org/gnome/desktop/notifications/application/zoom" = {
-      application-id = "Zoom.desktop";
-    };
-
     "org/gnome/desktop/peripherals/mouse" = {
       natural-scroll = false;
     };
@@ -81,85 +40,6 @@ in {
       natural-scroll = true;
       tap-to-click = true;
       two-finger-scrolling-enabled = true;
-    };
-
-    "org/gnome/desktop/privacy" = {
-      disable-microphone = false;
-      report-technical-problems = true;
-    };
-
-    "org/gnome/desktop/screensaver" = {
-      lock-delay = "uint32 0";
-      lock-enabled = false;
-    };
-
-    "org/gnome/desktop/search-providers" = {
-      sort-order = ["org.gnome.Contacts.desktop" "org.gnome.Documents.desktop" "org.gnome.Nautilus.desktop"];
-    };
-
-    "org/gnome/desktop/session" = {
-      idle-delay = "uint32 0";
-    };
-
-    "org/gnome/desktop/wm/keybindings" = {
-      close = ["<Super>q"];
-      switch-applications = "@as []";
-      switch-applications-backward = "@as []";
-      switch-to-workspace-1 = ["<Super>1"];
-      switch-to-workspace-10 = ["<Super>0"];
-      switch-to-workspace-2 = ["<Super>2"];
-      switch-to-workspace-3 = ["<Super>3"];
-      switch-to-workspace-4 = ["<Super>4"];
-      switch-to-workspace-5 = ["<Super>5"];
-      switch-to-workspace-6 = ["<Super>6"];
-      switch-to-workspace-7 = ["<Super>7"];
-      switch-to-workspace-8 = ["<Super>8"];
-      switch-to-workspace-9 = ["<Super>9"];
-      switch-windows = ["<Super>Tab"];
-      switch-windows-backward = ["<Shift><Super>Tab"];
-    };
-
-    "org/gnome/desktop/wm/preferences" = {
-      num-workspaces = 10;
-    };
-
-    "org/gnome/evolution-data-server" = {
-      migrated = true;
-      network-monitor-gio-name = "";
-    };
-
-    "org/gnome/gnome-system-monitor" = {
-      cpu-smooth-graph = true;
-      cpu-stacked-area-chart = false;
-      current-tab = "processes";
-      maximized = false;
-      network-total-in-bits = false;
-      show-dependencies = false;
-      show-whose-processes = "user";
-      solaris-mode = false;
-      window-state = mkTuple [1438 621];
-    };
-
-    "org/gnome/mutter" = {
-      attach-modal-dialogs = true;
-      center-new-windows = false;
-      dynamic-workspaces = true;
-      edge-tiling = true;
-      focus-change-on-pointer-rest = true;
-      workspaces-only-on-primary = false;
-    };
-
-    "org/gnome/nautilus/preferences" = {
-      default-folder-viewer = "icon-view";
-      search-filter-time-type = "last_modified";
-    };
-
-    "org/gnome/nautilus/window-state" = {
-      maximized = true;
-    };
-
-    "org/gnome/photos" = {
-      window-maximized = true;
     };
 
     "org/gnome/settings-daemon/plugins/color" = {
@@ -179,15 +59,31 @@ in {
 
     "org/gnome/shell" = {
       disable-user-extensions = false;
-      disabled-extensions = ["apps-menu@gnome-shell-extensions.gcampax.github.com" "native-window-placement@gnome-shell-extensions.gcampax.github.com" "drive-menu@gnome-shell-extensions.gcampax.github.com" "window-list@gnome-shell-extensions.gcampax.github.com" "auto-move-windows@gnome-shell-extensions.gcampax.github.com" "workspace-indicator@gnome-shell-extensions.gcampax.github.com"];
-      enabled-extensions = ["appindicatorsupport@rgcjonas.gmail.com" "x11gestures@joseexposito.github.io" "dash-to-dock@micxgx.gmail.com" "Vitals@CoreCoding.com"];
-      favorite-apps = ["firefox.desktop" "org.gnome.Console.desktop" "slack.desktop" "thunderbird.desktop" "todoist-electron.desktop" "spotify.desktop" "signal-desktop.desktop"];
+      disabled-extensions = [
+        "apps-menu@gnome-shell-extensions.gcampax.github.com"
+        "native-window-placement@gnome-shell-extensions.gcampax.github.com"
+        "drive-menu@gnome-shell-extensions.gcampax.github.com"
+        "window-list@gnome-shell-extensions.gcampax.github.com"
+        "auto-move-windows@gnome-shell-extensions.gcampax.github.com"
+        "workspace-indicator@gnome-shell-extensions.gcampax.github.com"
+      ];
+      enabled-extensions = [
+        "appindicatorsupport@rgcjonas.gmail.com"
+        "x11gestures@joseexposito.github.io"
+        "dash-to-dock@micxgx.gmail.com"
+        "Vitals@CoreCoding.com"
+      ];
+      favorite-apps = [
+        "firefox.desktop"
+        "org.gnome.Console.desktop"
+        "slack.desktop"
+        "thunderbird.desktop"
+        "todoist-electron.desktop"
+        "spotify.desktop"
+        "signal-desktop.desktop"
+      ];
       had-bluetooth-devices-setup = true;
       welcome-dialog-last-shown-version = "40.1";
-    };
-
-    "org/gnome/shell/extensions/auto-move-windows" = {
-      application-list = ["Alacritty.desktop:2" "firefox.desktop:1" "spotify.desktop:5" "slack.desktop:3" "thunderbird.desktop:4"];
     };
 
     "org/gnome/shell/extensions/dash-to-dock" = {
@@ -222,31 +118,8 @@ in {
       locations = "@av []";
     };
 
-    "org/gnome/system/location" = {
-      enabled = false;
-    };
-
     "org/gnome/tweaks" = {
       show-extensions-notice = false;
-    };
-
-    "org/gtk/settings/file-chooser" = {
-      date-format = "regular";
-      location-mode = "path-bar";
-      show-hidden = false;
-      show-size-column = true;
-      show-type-column = true;
-      sidebar-width = 171;
-      sort-column = "modified";
-      sort-directories-first = false;
-      sort-order = "descending";
-      type-format = "category";
-      window-position = mkTuple [26 23];
-      window-size = mkTuple [1203 958];
-    };
-
-    "system/proxy" = {
-      mode = "none";
     };
   };
 }
