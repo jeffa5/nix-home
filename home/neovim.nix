b5751a1a62ec005fb3a26718f5fb1de265df60a6
Andrew Jeffery <dev@jeffas.io>
2022-11-28 17:38:31 +0000

    Remove zathura from vimtex config

--- home/neovim.nix
+++ home/neovim.nix
@@ -101,7 +101,6 @@ pkgs: {
       {
         plugin = vimtex;
         config = ''
-          let g:vimtex_view_method = 'zathura'
           let g:vimtex_quickfix_open_on_warning = 0
           let g:vimtex_compiler_latexmk = {
               \ 'options' : [
