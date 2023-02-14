947c19eb96ca6b5f378414e58667a3e0dc8cc015
Andrew Jeffery <dev@jeffas.io>
2023-02-14 08:23:19 +0000

    Clear out nvidia config

--- nixos/xps-15/default.nix
+++ nixos/xps-15/default.nix
@@ -1,11 +1,4 @@
 {pkgs, ...}: let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec -a "$0" "$@"
-  '';
 in {
   imports = [
     ./hardware-configuration.nix
@@ -18,18 +11,6 @@ in {
     version = 2;
     efiSupport = true;
     device = "nodev";
-  };
-
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia.modesetting.enable = true;
-  environment.systemPackages = [nvidia-offload];
-
-  hardware.nvidia.prime = {
-    offload.enable = true;
-
-    intelBusId = "PCI:00:02:0";
-
-    nvidiaBusId = "PCI:01:00:0";
   };
 
   networking.hostName = "xps-15";
