3255326b435788b08a30299b405767924872aa35
Andrew Jeffery <dev@jeffas.io>
2023-07-19 21:25:29 +0100

    Set stateversion for rosebud

--- nixos/rosebud/default.nix
+++ nixos/rosebud/default.nix
@@ -12,4 +12,6 @@
   users.users.root.openssh.authorizedKeys.keys = [
     ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDd41siljusZNnvscLFujgkNiXM/qFpcaMdKwxC0GEm+980XSfH6P+FyUcuGntnrHcUExjhm2L6JoHbsz992PyGSqlb4mtcdwqdbCuTJWGo5lk4JJu/s74m1UGwGvSTwgv/R4UmBWCGfsJFdE8t1Oc2wmsHDbI6xRKyBnG8/zl3NFX7jKw0Ayz2LWIJdqW/FqRmOi6VdCcglRRmh1naAVcElBf2o/l4DRlvngIwOkhOHe3uy/foCVXW4/YvsIXsxuXlWrgGDz7MtR3ZBqqaSM4ORxLBG4Gpy80AlISRYru1FXWWG0S1rvs9jw6qcBC0xCfA+3fhoykvytuA/gjOxJLOQK5r8otUZxi72DveAEbr5RTk3w0PfUeyjGJnXZIpD9h7jLUFpDZJQn8Ln1Iw2edPnb4rdky0vLvYThbOPK+qhuUO2l4RuhDnfncXJYX5RMyXi+LrxSYx62mUv/nZbz1L6XVYAN3nZijtZHFXw4Rg9m1JZbFB0bkLbowW6q4lEL0=''
   ];
+
+  system.stateVersion = "23.05";
 }
