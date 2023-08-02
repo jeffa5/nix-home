960dac1d15f28b4ae4e0d1f21c9d61f093a8a865
Andrew Jeffery <dev@jeffas.io>
2023-08-02 15:28:38 +0100

    Don't use rightcert for cl vpn

--- nixos/default.nix
+++ nixos/default.nix
@@ -126,7 +126,7 @@ in {
         leftfirewall = "yes";
         right = "vpn2.cl.cam.ac.uk";
         rightid = "%any";
-        rightcert = toString ./cambridge-cl-vpn-2023.pem;
+        rightsendcert = "never";
         rightsubnet = "128.232.0.0/16,129.169.0.0/16,131.111.0.0/16,192.18.195.0/24,193.60.80.0/20,193.63.252.0/23,172.16.0.0/13,172.24.0.0/14,172.28.0.0/15,172.30.0.0/16,10.128.0.0/9,10.64.0.0/10,2001:630:210::/44,2a05:b400::/32";
         auto = "add";
       };
