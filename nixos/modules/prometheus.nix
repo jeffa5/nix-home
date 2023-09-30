3e0396251a8b955b35890efb63dcbc68c62d7d5a
Andrew Jeffery <dev@jeffas.io>
2023-09-30 11:26:06 +0100

    Add scrapeConfigs for prometheus

--- nixos/modules/prometheus.nix
+++ nixos/modules/prometheus.nix
@@ -1,6 +1,15 @@
-{...}: {
+{config, ...}: {
   services.prometheus = {
     enable = true;
     port = 9090;
+
+    scrapeConfigs = [
+      {
+        job_name = "node-exporters";
+        static_configs = [
+          {targets = ["127.0.0.1:${toString config.services.prometheus.exporters.node.port}"];}
+        ];
+      }
+    ];
   };
 }
