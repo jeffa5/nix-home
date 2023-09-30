0824fe20f6420729c8f07df1bdd09a316840362b
Andrew Jeffery <dev@jeffas.io>
2023-09-30 12:52:12 +0100

    Rename node-exporters job

--- nixos/modules/prometheus.nix
+++ nixos/modules/prometheus.nix
@@ -8,7 +8,7 @@ in {
 
     scrapeConfigs = [
       {
-        job_name = "node-exporters";
+        job_name = "node-exporter";
         static_configs = [
           {targets = ["127.0.0.1:${toString config.services.prometheus.exporters.node.port}"];}
         ];
