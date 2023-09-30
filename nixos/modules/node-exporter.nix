9fddc5131cca0eb706123c21379e3ed16d056850
Andrew Jeffery <dev@jeffas.io>
2023-09-30 12:39:27 +0100

    Add activation script for system version node exporter

--- nixos/modules/node-exporter.nix
+++ nixos/modules/node-exporter.nix
@@ -1,6 +1,25 @@
-{...}: {
+{...}: let
+  textFilesDir = "/var/lib/prometheus-node-exporter-text-files";
+in {
   services.prometheus.exporters.node = {
     enable = true;
-    enabledCollectors = ["systemd"];
+    enabledCollectors = ["systemd" "textfile"];
+    extraFlags = [
+      "--collector.textfile.directory=${textFilesDir}"
+    ];
   };
+
+  # https://grahamc.com/blog/nixos-system-version-prometheus/
+  # populate the textfile directory with the current system version on every boot and deployment
+  system.activationScripts.node-exporter-system-version = ''
+    mkdir -pm 0775 ${textFilesDir}
+    (
+      cd ${textFilesDir}
+      (
+        echo -n "system_version ";
+        readlink /nix/var/nix/profiles/system | cut -d- -f2
+      ) > system-version.prom.next
+      mv system-version.prom.next system-version.prom
+    )
+  '';
 }
