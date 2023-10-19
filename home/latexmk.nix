a63e7c586e94b6732701ee54be9b94b696ef29f0
Andrew Jeffery <dev@jeffas.io>
2023-10-19 13:33:45 +0100

    Update latexmk to use zathura

--- home/latexmk.nix
+++ home/latexmk.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   xdg.configFile."latexmk/latexmkrc".text = ''
-    $pdf_previewer = '${pkgs.evince}/bin/evince';
+    $pdf_previewer = '${pkgs.zathura}/bin/zathura';
     $pdf_mode = 1;
   '';
 }
