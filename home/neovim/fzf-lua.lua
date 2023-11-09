f11d867fe1a5804877416fe7b87b85214dc2036b
Andrew Jeffery <dev@jeffas.io>
2023-11-09 14:29:15 +0000

    Change blines to lines search

--- home/neovim/fzf-lua.lua
+++ home/neovim/fzf-lua.lua
@@ -3,7 +3,7 @@ local fzflua = require('fzf-lua')
 vim.keymap.set('n', '<leader>f', fzflua.files, { desc = "Find files" })
 vim.keymap.set('n', '<leader>l', fzflua.live_grep, { desc = "Find lines" })
 vim.keymap.set('n', '<leader>b', fzflua.buffers, { desc = "Buffers" })
-vim.keymap.set('n', '<leader>/', fzflua.blines, { desc = "Buffer lines" })
+vim.keymap.set('n', '<leader>/', fzflua.lines, { desc = "Buffer lines" })
 
 -- diagnostic mappings
 -- see `:help vim.diagnostic.*` for docs
