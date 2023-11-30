a818044320573584890c94fe31e01bd3eb5d56de
Andrew Jeffery <dev@jeffas.io>
2023-11-30 19:34:31 +0000

    Update recoll excludes

--- home/modules/recoll.nix
+++ home/modules/recoll.nix
@@ -1,5 +1,28 @@
 {...}: {
   services.recoll = {
     enable = true;
+    settings = {
+      cachedir = "~/.cache/recoll";
+      "skippedNames+" = [
+        ".cache"
+        "node_modules" # node
+        ".bundle" # ruby
+        ".cargo" # rust
+        "target" # rust
+        ".rustup" # rust
+        "result" # nix
+        "build" # misc
+        "3rdparty" # deps
+        ".mypy_cache" # python
+        ".venv" # python
+        ".env" # python
+        ".direnv" # python
+        ".npm" # nodejs
+        ".julia" # julia
+        "_opam" # ocaml
+        "_build" # ocaml
+        "Steam" # steam
+      ];
+    };
   };
 }
