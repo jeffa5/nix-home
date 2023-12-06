43af7259000e155c7b4a575d26b53deb742c9f09
Andrew Jeffery <dev@jeffas.io>
2023-12-06 16:47:59 +0000

    Add aerial outlines

--- home/neovim.nix
+++ home/neovim.nix
@@ -221,6 +221,22 @@
         '';
       }
       typst-vim
+
+      {
+        plugin = aerial-nvim;
+        type = "lua";
+        config = ''
+          -- aerial
+          require("aerial").setup({
+            on_attach = function(bufnr)
+              -- Jump forwards/backwards with '{' and '}'
+              vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+              vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+            end,
+          })
+          vim.keymap.set("n", "<localleader>o", "<cmd>AerialToggle!<CR>", {desc = "Outline"})
+        '';
+      }
     ];
   };
 }
