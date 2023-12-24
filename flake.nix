9892bc13f9e8887b9aa98c7a59de00b56e465557
Andrew Jeffery <dev@jeffas.io>
2023-12-24 17:28:59 +0000

    Update stablenixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    stableNixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    stableNixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
