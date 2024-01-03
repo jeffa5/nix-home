9c15943d6d5c5fac5d1a00131cd064115ac72f04
Andrew Jeffery <dev@jeffas.io>
2024-01-03 09:23:17 +0000

    Update username

--- home/modules/contact-accounts.nix
+++ home/modules/contact-accounts.nix
@@ -2,6 +2,6 @@
   jeffas = {
     address = "andrew@jeffas.io";
     url = "https://cloud.jeffas.net/remote.php/dav/addressbooks/users/Admin/contacts/";
-    userName = "Admin";
+    userName = "admin";
   };
 }
