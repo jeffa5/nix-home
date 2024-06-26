c3678f6be8a4f4d3429fee6b7864e1b4a98c247f
Andrew Jeffery <dev@jeffas.net>
2024-06-26 10:03:29 +0200

    Update flake to stable 24.05

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager?ref=release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hardware.url = "github:nixos/nixos-hardware";
