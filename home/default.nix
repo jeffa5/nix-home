f72ccd8b4afc0c2dc5ed5043b6855670a6b11d40
Andrew Jeffery <dev@jeffas.net>
2024-10-01 06:48:37 +0100

    Add mime type for docx

--- home/default.nix
+++ home/default.nix
@@ -79,6 +79,7 @@ in {
       "x-scheme-handler/https" = ["firefox.desktop"];
       "x-scheme-handler/about" = ["firefox.desktop"];
       "x-scheme-handler/unknown" = ["firefox.desktop"];
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = ["calc.desktop"];
     };
   };
 
