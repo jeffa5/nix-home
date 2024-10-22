fb13f95841b6ea753b8b425f0e769c9d8a345548
Andrew Jeffery <dev@jeffas.net>
2024-10-22 20:33:07 +0100

    Fix mbsync updates

--- home/modules/mbsync.nix
+++ home/modules/mbsync.nix
@@ -1,8 +1,18 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  mu = lib.getExe pkgs.mu;
+  muUpdate = pkgs.writeShellScriptBin "mu-update" ''
+    ${mu} index
+    ${mu} cfind > ~/contacts/list
+  '';
+in {
   programs.mbsync.enable = true;
 
   services.mbsync = {
     enable = true;
-    postExec = "${pkgs.mu}/bin/mu index && ${pkgs.mu}/bin/mu cfind > ~/contacts/list";
+    postExec = lib.getExe muUpdate;
   };
 }
