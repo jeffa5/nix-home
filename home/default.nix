7205f9c0f92850abee43abe891dd6b953b5b085f
Andrew Jeffery <dev@jeffas.net>
2024-12-30 20:47:43 +0900

    Set aerc as mailto handler in xdg-mime

--- home/default.nix
+++ home/default.nix
@@ -85,6 +85,7 @@ in {
       "x-scheme-handler/https" = ["firefox.desktop"];
       "x-scheme-handler/about" = ["firefox.desktop"];
       "x-scheme-handler/unknown" = ["firefox.desktop"];
+      "x-scheme-handler/mailto" = ["aerc.desktop"];
       "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = ["writer.desktop"];
       "application/vnd.oasis.opendocument.text" = ["writer.desktop"];
       "application/vnd.oasis.opendocument.spreadsheet" = ["calc.desktop"];
