0a7f30b0d6ac138a288d559677b8e6dd77a1d165
Andrew Jeffery <dev@jeffas.net>
2025-03-28 09:31:22 +0000

    Remove rpi1 dns server

--- nixos/modules/tailscale.nix
+++ nixos/modules/tailscale.nix
@@ -1,8 +1,6 @@
 {...}: {
   services.tailscale.enable = true;
 
-  networking.resolvconf.extraConfig = ''
-    name_servers='100.106.84.46'
-  '';
-  networking.nameservers = ["100.106.84.46"];
+  networking.resolvconf.extraConfig = "";
+  networking.nameservers = [];
 }
