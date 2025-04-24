a5d7e11d2429fce3c584c806886a8d233335250d
Andrew Jeffery <dev@jeffas.net>
2025-04-24 12:41:36 +0100

    Use stablenixpkgs

--- flake.nix
+++ flake.nix
@@ -167,7 +167,7 @@
         system = "aarch64-linux";
         modules = [
           hardware.nixosModules.raspberry-pi-4
-          (import ./nixos/rpi0 {inherit nixpkgs;})
+          (import ./nixos/rpi0 {nixpkgs = stableNixpkgs;})
         ];
       };
 
