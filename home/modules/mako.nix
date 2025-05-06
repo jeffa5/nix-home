4055e31ad0205d467c5b2786053ea3979b1292b5
Andrew Jeffery <ajeffery@cloudflare.com>
2025-05-06 11:39:44 +0100

    Update and fix mako

--- home/modules/mako.nix
+++ home/modules/mako.nix
@@ -1,29 +1,32 @@
 {...}: {
   services.mako = {
     enable = true;
-    backgroundColor = "#282828";
-    borderSize = 4;
-    defaultTimeout = 10000;
-    font = "hack 12";
-    height = 200;
-    textColor = "#ebdbb2";
-    width = 400;
-    format = "<b><u>%a</u></b>\\n<b>%s</b>\\n%b";
-    extraConfig = ''
-      [hidden]
-      border-color=#83a598
-
-      [urgency=low]
-      border-color=#b8bb26
-
-      [urgency=normal]
-      border-color=#fabd2f
-
-      [urgency=high]
-      border-color=#fb4934
-
-      [mode=dnd]
-      invisible=1
-    '';
+    settings = {
+      backgroundColor = "#282828";
+      borderSize = 4;
+      defaultTimeout = 10000;
+      font = "hack 12";
+      height = 200;
+      textColor = "#ebdbb2";
+      width = 400;
+      format = "<b><u>%a</u></b>\\n<b>%s</b>\\n%b";
+    };
+    criteria = {
+      "hidden" = {
+        border-color = "#83a598";
+      };
+      "urgency=low" = {
+        border-color = "#b8bb26";
+      };
+      "urgency=normal" = {
+        border-color = "#fabd2f";
+      };
+      "urgency=high" = {
+        border-color = "#fb4934";
+      };
+      "mode=dnd" = {
+        invisible = "1";
+      };
+    };
   };
 }
