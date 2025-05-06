4eb439b2d6781b9b006911d7a16558362405ea46
Andrew Jeffery <ajeffery@cloudflare.com>
2025-05-06 11:19:27 +0100

    Add us colemak-dh layout

--- home/xkb.nix
+++ home/xkb.nix
@@ -29,4 +29,35 @@
       key <AB07> { [ h, H ] };
     };
   '';
+
+  xdg.configFile."xkb/symbols/iso-us-colemak-dh".text = ''
+    partial alphanumeric_keys
+    default xkb_symbols "colemak-dh" {
+      include "us"
+      name[Group1] = "US (Colemak DH)";
+
+      key <AD03> { [ f, F ] };
+      key <AD04> { [ p, P ] };
+      key <AD05> { [ b, B ] };
+      key <AD06> { [ j, J ] };
+      key <AD07> { [ l, L ] };
+      key <AD08> { [ u, U ] };
+      key <AD09> { [ y, Y ] };
+      key <AD10> { [ semicolon, colon ] };
+
+      key <AC02> { [ r, R ] };
+      key <AC03> { [ s, S ] };
+      key <AC04> { [ t, T ] };
+      key <AC06> { [ m, M ] };
+      key <AC07> { [ n, N ] };
+      key <AC08> { [ e, E ] };
+      key <AC09> { [ i, I ] };
+      key <AC10> { [ o, O ] };
+
+      key <AB04> { [ d, D ] };
+      key <AB05> { [ v, V ] };
+      key <AB06> { [ k, K ] };
+      key <AB07> { [ h, H ] };
+    };
+  '';
 }
--- packages/colemakdh/builder.sh
+++ packages/colemakdh/builder.sh
@@ -1,3 +1,4 @@
 export PATH="$coreutils/bin:$gzip/bin"
 mkdir -p $out/share/keymaps/i386/colemak
 gzip --stdout $src/iso-uk-colemak-dh.map > $out/share/keymaps/i386/colemak/iso-uk-colemak-dh.map.gz
+gzip --stdout $src/iso-us-colemak-dh.map > $out/share/keymaps/i386/colemak/iso-us-colemak-dh.map.gz
