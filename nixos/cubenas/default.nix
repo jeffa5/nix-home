aa2616db464891a222c6a9af06332885fede0e56
Andrew Jeffery <dev@jeffas.net>
2025-06-26 19:07:19 +0100

    Add autoscrub for btrfs

--- nixos/cubenas/default.nix
+++ nixos/cubenas/default.nix
@@ -8,6 +8,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ../modules/btrfs.nix
     # (import ../modules/nix.nix {
     #   inherit nixpkgs;
     #   users = [];
