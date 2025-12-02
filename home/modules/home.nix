3f7c7900a9f8679a3335c3cc87f913dd0d68c8f2
Andrew Jeffery <dev@jeffas.net>
2025-12-02 11:29:04 -0800

    Make shell aliases common and add env vars to nu

--- home/modules/home.nix
+++ home/modules/home.nix
@@ -10,6 +10,31 @@ in {
       EDITOR = "nvim";
     };
 
+    shellAliases = {
+      cat = "bat";
+      g = "git";
+      ga = "git add";
+      gc = "git commit -v";
+      gd = "git diff";
+      gdm = "git diff main";
+      gst = "git status";
+      gl = "git pull";
+      glum = "git pull upstream main";
+      gp = "git push";
+      gpu = "git push --set-upstream origin HEAD";
+      gpf = "git push --force-with-lease";
+      gcm = "git checkout main";
+      grb = "git rebase";
+      grbm = "git rebase main";
+      grbc = "git rebase --continue";
+      grba = "git rebase --abort";
+      gsh = "git show";
+      glog = "git log --oneline --graph";
+      gco = "git checkout";
+      gcb = "git checkout -b";
+      gstp = "git stash pop";
+    };
+
     stateVersion = "21.11";
   };
   programs.home-manager.enable = true;
--- home/modules/nushell.nix
+++ home/modules/nushell.nix
@@ -1,31 +1,8 @@
-{...}: {
+{config, ...}: {
   programs.nushell = {
     enable = true;
 
-    shellAliases = {
-      cat = "bat";
-      g = "git";
-      ga = "git add";
-      gc = "git commit -v";
-      gd = "git diff";
-      gdm = "git diff main";
-      gst = "git status";
-      gl = "git pull";
-      glum = "git pull upstream main";
-      gp = "git push";
-      gpu = "git push --set-upstream origin HEAD";
-      gpf = "git push --force-with-lease";
-      gcm = "git checkout main";
-      grb = "git rebase";
-      grbm = "git rebase main";
-      grbc = "git rebase --continue";
-      grba = "git rebase --abort";
-      gsh = "git show";
-      glog = "git log --oneline --graph";
-      gco = "git checkout";
-      gcb = "git checkout -b";
-      gstp = "git stash pop";
-    };
+    environmentVariables = config.home.sessionVariables;
 
     settings = {
       show_banner = false;
