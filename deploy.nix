5cb0b83d4e5ca0eb0ed394a88a16b9aa329b13cd
Andrew Jeffery <dev@jeffas.net>
2025-12-03 08:38:43 -0800

    Update deploy script to use nh

--- deploy.nix
+++ deploy.nix
@@ -4,7 +4,7 @@
   hosts,
 }: let
   deploy = host: ip: let
-    cmd = "nixos-rebuild switch --flake .#${host} --show-trace --no-reexec --build-host root@${ip} --target-host root@${ip}";
+    cmd = "nh os switch .# --hostname ${host} --show-trace --build-host root@${ip} --target-host root@${ip}";
   in ''
     echo "+ ${cmd}"
     ${cmd}
