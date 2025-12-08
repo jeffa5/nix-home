aa483240ad5894c5e7153833121c64000f1aa82d
Andrew Jeffery <dev@jeffas.net>
2025-12-07 21:39:58 -0800

    Add syncthing package to env

--- nixos/modules/syncthing.nix
+++ nixos/modules/syncthing.nix
@@ -1,6 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   services.syncthing = {
     enable = true;
     openDefaultPorts = true; # Open ports in the firewall for Syncthing. (NOTE: this will not open syncthing gui port)
   };
+
+  environment.systemPackages = [pkgs.syncthing];
 }
