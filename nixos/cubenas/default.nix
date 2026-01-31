6361513951bdf266e0590a672aaea2099c6ad977
Andrew Jeffery <dev@jeffas.net>
2026-01-31 16:22:20 +0000

    Set up basic home assistant

--- nixos/cubenas/default.nix
+++ nixos/cubenas/default.nix
@@ -58,6 +58,7 @@
 
     ./files.nix
     ./stafil.nix
+    ./home-assistant.nix
   ];
 
   programs.tmux.enable = true;
---
+++ nixos/cubenas/home-assistant.nix
@@ -0,0 +1,64 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  services.home-assistant = {
+    enable = true;
+    extraPackages = ps: [
+      ps.psycopg2 # for postgresql
+    ];
+    extraComponents = [
+      "default_config"
+      "esphome"
+      "google_translate"
+      "isal"
+      "met"
+      "mobile_app"
+      "mqtt"
+      "radio_browser"
+      "shopping_list"
+    ];
+    config = {
+      # Includes dependencies for a basic setup
+      # https://www.home-assistant.io/integrations/default_config/
+      default_config = {};
+
+      recorder.db_url = "postgresql://@/hass";
+      http = {
+        server_host = "127.0.0.1";
+        trusted_proxies = ["127.0.0.1"];
+        use_x_forwarded_for = true;
+      };
+    };
+  };
+
+  services.postgresql = {
+    ensureDatabases = ["hass"];
+    ensureUsers = [
+      {
+        name = "hass";
+        ensureDBOwnership = true;
+      }
+    ];
+  };
+
+  services.nginx.virtualHosts."Home Assistant" = let
+    authelia-snippets = import ../modules/authelia-snippets.nix {inherit pkgs;};
+  in {
+    serverName = "hass.home.jeffas.net";
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.home-assistant.config.http.server_port}";
+      proxyWebsockets = true;
+      extraConfig = ''
+        include ${authelia-snippets.proxy};
+        include ${authelia-snippets.authelia-authrequest};
+      '';
+    };
+    forceSSL = true;
+    useACMEHost = "home.jeffas.net";
+    extraConfig = ''
+      include ${authelia-snippets.authelia-location};
+    '';
+  };
+}
