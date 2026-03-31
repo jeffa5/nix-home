57ec8e9f9627974c98a1cae6d7c537cfd4ca3e85
Andrew Jeffery <dev@jeffas.net>
2026-03-31 08:30:34 +0100

    Increase stafil concurrency

--- nixos/cubenas/stafil.nix
+++ nixos/cubenas/stafil.nix
@@ -223,13 +223,15 @@ in {
 
   users.groups.${group} = {};
 
-  systemd.services.stafil = {
+  systemd.services.stafil = let
+    concurrency = "8";
+  in {
     enable = true;
     description = "Generate stafil file browser";
     script = ''
       set -ex
-      time ${lib.getExe' pkgs.stafil "stafil-configure"} --config-file ${configFile} --search-paths ${destination}/search.paths --concurrency 2 >${destination}/build.ninja
-      time ${lib.getExe pkgs.ninja} -C ${destination} -j 2 -k 0
+      time ${lib.getExe' pkgs.stafil "stafil-configure"} --config-file ${configFile} --search-paths ${destination}/search.paths --concurrency ${concurrency} >${destination}/build.ninja
+      time ${lib.getExe pkgs.ninja} -C ${destination} -j ${concurrency} -k 0
     '';
     environment = {
       SHELL = lib.getExe pkgs.bash;
